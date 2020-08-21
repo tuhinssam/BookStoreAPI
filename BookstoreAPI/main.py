808eb5e2ae35fcedadb6a2a05e08e4dba46b7363
tuhinssam <tuhinssam@gmail.com>
2020-08-21 23:28:22 +0530

    3 APIs added queried Get, Patch

--- BookstoreAPI/main.py
+++ BookstoreAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from models.user import User
 
 app = FastAPI()
@@ -35,3 +35,31 @@ Endpoint: http://127.0.0.1:8000/user?password=pass1
 @app.get('/user')
 async def get_user_validation(password:str):
     return {"query parameter": password}
+
+'''
+Method: GET
+http://127.0.0.1:8000/book/isbn1
+'''
+@app.get("/book/{isbn}")
+async def get_book_with_isbn(isbn: str):
+    return {"isbn of the book": isbn}
+
+'''
+Method: GET
+http://127.0.0.1:8000/author/2/book?order=order1&category=category1
+'''
+@app.get("/author/{id}/book")
+async def get_author_books(id: int, category: str, order: str= 'asc'):
+    return {"Query Changable parameter": order + category + str(id)}
+
+'''
+Method: Patch
+http://127.0.0.1:8000/author/name
+body:
+{
+  "name":"tuhin"
+}
+'''
+@app.patch('/author/name')
+async def patch_author_name(name: str = Body(..., embed = True)):
+    return {"name in body": name}
